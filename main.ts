3734e11ab5c49004a01c826106121fbc104f3f5f
attilamakerhu <87415155+attilamakerhu@users.noreply.github.com>
2022-02-21 12:53:24 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("Ada", 1815)
 })
+radio.onReceivedValue(function (name, value) {
+    basic.showString(name)
+    basic.showNumber(value)
+})
+radio.setGroup(42)
